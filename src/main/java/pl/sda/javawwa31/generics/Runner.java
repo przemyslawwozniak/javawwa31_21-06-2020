e884dad509fda9232a754690122b52ef79ad35cc
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-06-27 11:56:06 +0200

    Student, Comparator, Comparable - final commit

--- src/main/java/pl/sda/javawwa31/generics/Runner.java
+++ src/main/java/pl/sda/javawwa31/generics/Runner.java
@@ -1,7 +1,6 @@
 package pl.sda.javawwa31.generics;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Runner {
 
@@ -26,6 +25,33 @@ public class Runner {
         Student s3 = new Student("Robert", "Gwiazdowski", 4, 4.75);
         Student s4 = new Student("Krzystof", "Czech", 1, 3.0);
         Student s5 = new Student("Adrian", "Polak", 3, 3.25);
+
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+
+        System.out.println("Przed sortowaniem: " + students);
+
+        Collections.sort(students);
+        System.out.println("Po sortowaniu wg naturalnego porzadku (Comparable): " + students);
+
+        Comparator<Student> customComparator = new StudentEquationComparator();
+        Collections.sort(students, customComparator);
+        System.out.println("Po sortowaniu wg ustalonego porzadku (Comparator): " + students);
+    }
+
+    public static class StudentEquationComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return (int)(eq(s1) - eq(s2));
+        }
+
+        private double eq(Student s) {
+            return s.getLastName().length() + s.getYear() * s.getAvg();
+        }
     }
 
 }
--- src/main/java/pl/sda/javawwa31/generics/Student.java
+++ src/main/java/pl/sda/javawwa31/generics/Student.java
@@ -1,6 +1,6 @@
 package pl.sda.javawwa31.generics;
 
-public class Student {
+public class Student implements Comparable<Student> {
     private String firstName, lastName;
     private int year;
     private double avg;
@@ -10,6 +10,17 @@ public class Student {
         this.lastName = lastName;
         this.year = year;
         this.avg = avg;
+    }
+
+    //1.0
+    //1.75
+    //10*(1 - 1.75) = 10*0.75 = 7.5 -> 7 > 0
+    //1.0
+    //1.01
+    //1 - 1.01 = -0.01
+    @Override
+    public int compareTo(Student other) {
+        return (int)(100 * (this.avg - other.avg));
     }
 
     public String getFirstName() {
@@ -26,5 +37,15 @@ public class Student {
 
     public double getAvg() {
         return avg;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", year=" + year +
+                ", avg=" + avg +
+                '}';
     }
 }
