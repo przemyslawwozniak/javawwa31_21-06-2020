2b76fb45fa0717911d9ced73633d6e1cde75f7bb
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-06-27 11:08:45 +0200

    Pair

---
+++ src/main/java/pl/sda/javawwa31/generics/Runner.java
@@ -0,0 +1,10 @@
+package pl.sda.javawwa31.generics;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Pair<String, String> capitalCountry = new Pair<>("Warsaw", "Poland");
+        Pair<Integer, String> marketShare = new Pair<>(1, "Apple");
+    }
+
+}
