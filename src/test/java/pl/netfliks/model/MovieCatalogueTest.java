046bf0272e07c6464891614bc9ae9f303e765192
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-06-28 13:13:38 +0200

    Netfliks - zad 2 - TAD intro

---
+++ src/test/java/pl/netfliks/model/MovieCatalogueTest.java
@@ -0,0 +1,22 @@
+package pl.netfliks.model;
+
+import org.junit.Test;
+
+public class MovieCatalogueTest {
+
+
+
+    @Test
+    public void access_resource_file() {
+        //given:
+
+
+        //when:
+
+
+        //then:
+
+
+    }
+
+}
